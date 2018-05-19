3283e5f0da17cd6fcdf8c430d9422c137a86a5c9
rogachevsn <rogachevsn55@gmail.com>
2018-05-19 11:32:33 +0300

    create Spring Boot application DIDResolver

---
+++ src/main/java/jp/co/soramitsu/sora/didresolver/DIDResolverApplication.java
@@ -0,0 +1,13 @@
+package jp.co.soramitsu.sora.didresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DIDResolverApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DIDResolverApplication.class, args);
+	}
+}
