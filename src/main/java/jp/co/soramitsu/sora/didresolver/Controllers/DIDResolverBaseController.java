9e318bb8252acc1680f110848e24422e4f5509e1
rogachevsn <rogachevsn55@gmail.com>
2018-05-30 19:15:32 +0300

    add validation to DID param, add Service element to DDO, change status code for DIDDuplicateException

--- src/main/java/jp/co/soramitsu/sora/didresolver/Controllers/DIDResolverBaseController.java
+++ src/main/java/jp/co/soramitsu/sora/didresolver/Controllers/DIDResolverBaseController.java
@@ -8,8 +8,10 @@ import jp.co.soramitsu.sora.didresolver.exceptions.DIDDuplicateException;
 import jp.co.soramitsu.sora.didresolver.services.StorageService;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
@@ -39,6 +41,12 @@ public class DIDResolverBaseController {
         storageService.createOrUpdate(ddo.getId(), ddo);
     }
 
+    /**
+     * check that the DID already exists in the storage
+     *
+     * @param did - valid URLEncoded DID
+     * @return true if DID already exists in the storage
+     */
     boolean checkDDOByDidAtStorage(String did) {
         return Optional.ofNullable(storageService.read(did)).isPresent();
     }
