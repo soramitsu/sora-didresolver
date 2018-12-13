140c41eb0fa24b6d5f3f4b9f68c79dce2ab11933
dorzhi <tarbaev@soramitsu.co.jp>
2018-12-13 12:22:31 +0300

    More sonar issues fixes

--- src/main/java/jp/co/soramitsu/sora/didresolver/validation/KeyValidator.java
+++ src/main/java/jp/co/soramitsu/sora/didresolver/validation/KeyValidator.java
@@ -1,5 +1,6 @@
 package jp.co.soramitsu.sora.didresolver.validation;
 
+import static java.util.Objects.nonNull;
 import static java.util.regex.Pattern.compile;
 
 import java.net.URI;
@@ -13,14 +14,14 @@ import lombok.extern.slf4j.Slf4j;
 public class KeyValidator implements ConstraintValidator<KeyConstraint, URI> {
 
   private static final Pattern COMPILED_PATTERN = compile("keys-\\d+");
+  private static final DIDValidator validator = new DIDValidator();
 
   @Override
   public boolean isValid(URI publicKey, ConstraintValidatorContext context) {
     log.debug("validation format of public key - {}", publicKey);
     String did = publicKey.getScheme() + ":" + publicKey.getSchemeSpecificPart();
     String fragment = publicKey.getFragment();
-    boolean isValid =
-        new DIDValidator().isValid(did, context) && fragment != null && COMPILED_PATTERN
+    boolean isValid = nonNull(fragment) && validator.isValid(did, context) && COMPILED_PATTERN
             .matcher(fragment).matches();
     log.debug("result of validation format of public key - {} is {}", publicKey, isValid);
     return isValid;
