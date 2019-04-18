84ff3930550fe0b1f4a2fb6b40e53418fce07c8d
rogachevsn <rogachevsn55@gmail.com>
2019-04-18 13:40:16 +0300

    SP-422: add usage of Consul to DIDResolver
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    # Conflicts:
    #	build.gradle
    #	deploy/docker-compose.yml

--- src/main/java/jp/co/soramitsu/sora/didresolver/DIDResolverApplication.java
+++ src/main/java/jp/co/soramitsu/sora/didresolver/DIDResolverApplication.java
@@ -2,8 +2,10 @@ package jp.co.soramitsu.sora.didresolver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DIDResolverApplication {
 
   public static void main(String[] args) {
