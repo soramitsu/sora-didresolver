180872d94746af9aa607d93a935e96af638410df
jiexa <malyshevalexey8@gmail.com>
2019-06-11 14:58:30 +0300

    add context path and API versioning

--- src/main/java/jp/co/soramitsu/sora/didresolver/commons/URIConstants.java
+++ src/main/java/jp/co/soramitsu/sora/didresolver/commons/URIConstants.java
@@ -1,10 +1,13 @@
 package jp.co.soramitsu.sora.didresolver.commons;
 
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
 public final class URIConstants {
 
-  public static final String PATH = "/did";
   public static final String ID_PARAM = "/{did}";
-
-  private URIConstants() {
-  }
+  private static final String V1 = "/v1";
+  public static final String PATH = V1 + "/did";
 }
